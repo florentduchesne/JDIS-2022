a6f432da50e2bf64f360a8081a18147048166a4a
Florent Duchesne <florentduchesne@hotmail.com>
2022-07-23 13:37:04 -0400

    fix upgrades + tank

--- upgrading.py
+++ upgrading.py
@@ -2,28 +2,28 @@ from core import Action, GameState, Upgrade, Debris, Tank
 import random
 
 
-def get_updgrade(our_tank : Tank) -> Upgrade:
-  if our_tank.fire_rate < 4:
+def get_upgrade(our_tank : Tank) -> Upgrade:
+  if our_tank.upgrade_levels.fire_rate < 4:
     return Upgrade.FIRE_RATE_UPGRADE
 
-  if our_tank.max_hp < 2:
+  if our_tank.upgrade_levels.max_hp < 2:
     return Upgrade.MAX_HP_UPGRADE
-  if our_tank.hp_regen < 2:
+  if our_tank.upgrade_levels.hp_regen < 2:
     return Upgrade.HP_REGEN
 
-  if our_tank.max_hp < 4:
+  if our_tank.upgrade_levels.max_hp < 4:
     return Upgrade.MAX_HP_UPGRADE
-  if our_tank.hp_regen < 4:
+  if our_tank.upgrade_levels.hp_regen < 4:
     return Upgrade.HP_REGEN
 
-  if our_tank.speed < 2:
+  if our_tank.upgrade_levels.speed < 2:
     return Upgrade.SPEED_UPGRADE
-  if our_tank.projectile_damage < 2:
+  if our_tank.upgrade_levels.projectile_damage < 2:
     return Upgrade.PROJECTILE_DAMAGE
  
-  if our_tank.speed < 4:
+  if our_tank.upgrade_levels.speed < 4:
     return Upgrade.SPEED_UPGRADE
-  if our_tank.projectile_damage < 4:
+  if our_tank.upgrade_levels.projectile_damage < 4:
     return Upgrade.PROJECTILE_DAMAGE
   
   return random.choice([
